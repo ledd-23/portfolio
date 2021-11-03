a0905a91a866bf96dab2983a49b984fcc5293e76
Danh Le <ledangdanh123@gmail.com>
2021-11-03 02:10:52 -0500

    added slide in effect

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import ProgressBar from 'react-bootstrap/ProgressBar';
@@ -6,12 +6,29 @@ import progress, {libraries} from "../progress-data";
 import Link from "./Link";
 import socials, {icons} from "../social-media";
 
+function FadeInSection(props) {
+  const [isVisible, setVisible] = React.useState(false);
+  const domRef = React.useRef();
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => setVisible(entry.isIntersecting));
+    });
+    observer.observe(domRef.current);
+  }, []);
+  return (
+    <div
+      className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+      ref={domRef}
+    >
+      {props.children}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
         <Header />
-
         <section id="title" className="title-section">
           <div className="container-fluid">
             <div className="row">
@@ -39,55 +56,70 @@ function App() {
         <section className="about-section corner">
           <div className="container-fluid">
             <div className="row mb-5" id="about">
-              <div className="col-lg-6">
-                <a href="https://en.wikipedia.org/wiki/%C4%90%C3%B4ng_H%E1%BB%93_painting">
-                  <img className="another-round-image" src="https://www.vietnam-culture.com/wp-content/uploads/2014/02/Dong-Ho-Painting.jpg"></img>
-                </a>
-              </div>
+                <div className="col-lg-6">
+                  <FadeInSection>
+                    <a href="https://en.wikipedia.org/wiki/%C4%90%C3%B4ng_H%E1%BB%93_painting">
+                      <img className="another-round-image" src="https://www.vietnam-culture.com/wp-content/uploads/2014/02/Dong-Ho-Painting.jpg"></img>
+                    </a>
+                  </FadeInSection>
+                </div>
               
               <div className="righty col-lg-6 px-5">
-                <h2>about me</h2>
-                <p>
-                  i was born in hà nội, việt nam.
-                  i play the piano. my favorite composer is chopin.
-                  i often play video games and occasionally basketball too.
-                </p>
-                <p>
-                  i fell in love with computer science in college.
-                  my passion is distributed and parallel systems, but
-                  im learning ui/ux desgin and drawing too. 
-                  i think they're very expressive.
-                </p>
+                <FadeInSection>
+                    <h2>about me</h2>
+                    <p>
+                      i was born in hà nội, việt nam.
+                      i play the piano. my favorite composer is chopin.
+                      i often play video games and occasionally basketball too.
+                    </p>
+                    <p>
+                      i fell in love with computer science in college.
+                      my passion is distributed and parallel systems, but
+                      im learning ui/ux desgin and drawing too. 
+                      i think they're very expressive.
+                    </p>
+                  </FadeInSection>
               </div>
             </div>
           
         
             <div id="skills" className="row">
-              <h2>skills</h2>
-              <div className="col-lg-6 corner pt-3">
-                <h5 className="pb-3">programming languages</h5>
-                {Object.keys(progress).map((skill, index) => {
-                          return (
-                            <div>
-                              <ProgressBar key={index} now={progress[skill]} />
-                              <p>{skill}</p>
-                            </div>
-                          )
-                })}
-              </div>
-              <div className="col-lg-6">
-                <div className="corner py-2">
-                  <h5 className="ps-3 pb-3 pt-2">libraries</h5>
-                  {Object.keys(libraries).map((skill, index) => {
-                            return (
-                              <div className="ps-3">
-                                <ProgressBar key={index} now={libraries[skill]} />
-                                <p>{skill}</p>
-                              </div>
-                            )
-                  })}
-                  </div>
-              </div>
+                <FadeInSection>
+                  <h2>skills</h2>
+                </FadeInSection>
+                <div className="col-lg-6">
+                  <FadeInSection>
+                    <div className="corner py-2">
+                      <h5 className="ps-3 pb-3 pt-2">programming languages</h5>
+                      {Object.keys(progress).map((skill, index) => {
+                                return (
+                                  <div className="ps-3">
+                                    <ProgressBar key={index} now={progress[skill]} />
+                                    <p>{skill}</p>
+                                  </div>
+                                )
+                      })}
+                    </div>
+                  </FadeInSection>
+                </div>
+              
+              
+                <div className="col-lg-6">
+                  <FadeInSection>
+                    <div className="corner py-2">
+                      <h5 className="ps-3 pb-3 pt-2">libraries</h5>
+                      {Object.keys(libraries).map((skill, index) => {
+                                return (
+                                  <div className="ps-3">
+                                      <ProgressBar key={index} now={libraries[skill]} />
+                                      <p>{skill}</p>
+                                  </div>
+                                )
+                      })}
+                      </div>
+                    </FadeInSection>
+                </div>
+             
             </div>
           </div>
         </section>
