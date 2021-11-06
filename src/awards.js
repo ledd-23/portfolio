13df10c0b3148d8d727a4d94e092d9f353b5019f
Danh Le <ledangdanh123@gmail.com>
2021-11-06 14:10:29 -0500

    add additional certificate

--- src/awards.js
+++ src/awards.js
@@ -6,7 +6,7 @@ const hackerranks = {
 
 const udemys = {
     "Kafka Cluster" : "https://www.udemy.com/certificate/UC-f4991766-a638-40c5-a0d4-ea8e1d0e32a6/",
-    "Web Developer" : ""
+    "Web Developer" : "https://www.udemy.com/certificate/UC-6d5b0064-5ee8-4a7d-8fd3-5c057437b8ce/"
 }
 
 export default hackerranks;
